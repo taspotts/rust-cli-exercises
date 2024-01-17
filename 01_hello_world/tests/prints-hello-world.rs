c328d278392188d6f5375491598bb642a2068027
Terry Spotts <terryspotts@mac.com>
2024-01-16 21:50:35 -0500

    01_hello_world complete!

--- 01_hello_world/tests/prints-hello-world.rs
+++ 01_hello_world/tests/prints-hello-world.rs
@@ -1,5 +1,8 @@
+use assert_cmd::Command;
+
 #[test]
-fn prints_hello_world() -> Result<(), Box<dyn std::error::Error>> { 
-    todo!("put your integration test code here!");
+fn prints_hello_world() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("hello_world").unwrap();
+    cmd.assert().stdout("Hello World!\n");
     Ok(())
 }
